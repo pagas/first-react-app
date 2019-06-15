3393fe3f2745692caf58d59a533f68980ba1a094
paulius <paulius@2simple.com>
2019-06-15 21:44:05 +0100

    add Coins

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../App.css';
 import Header from "../shared/components/layout/Header";
-//import Home from '../components/Home/Home';
+import Home from '../components/Home/Home';
 import Footer from "../shared/components/layout/Footer";
 import Content from "../shared/components/layout/Content";
-//import Todo from "./Todo/Todo";
+import Todo from "./Todo/Todo";
 import Timer from "./Pomodoro/Timer";
+import Coins from "./Coins/Coins";
 
 
 function App() {
@@ -14,7 +15,11 @@ function App() {
             <Header title="The new header title"/>
             <Content>
 
+
+                <Coins/>
                 <Timer/>
+                <Todo/>
+                <Home/>
             </Content>
             <Footer/>
         </div>
