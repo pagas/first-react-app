b026372d263183a2b290f5281b110106609d8062
paulius <paulius@2simple.com>
2019-06-15 13:25:28 +0100

    create local state for Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react';
 import './Home.css';
 
 export default class home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: 'Carlos'
+        }
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState({
+                name: 'Cristina'
+            })
+        })
+    }
+
     render() {
+        console.log('re-render is called');
         return <div className="Home">
             <h1>Welcome to Codejobs</h1>
 
+            <p>Hi my name is {this.state.name} </p>
             <p>
                 In this recipe you will learn how to add styles to
                 components. If you want to learn more you can visit
