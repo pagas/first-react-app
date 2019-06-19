8ed1ef340ada3d992e788beea7cf377cefb51e1c
paulius <paulius@2simple.com>
2019-06-19 20:53:15 +0100

    fix App eslint

--- src/components/App.js
+++ src/components/App.js
@@ -1,96 +1,98 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../App.css';
 import './Person/Popup.css';
-import Header from "../shared/components/layout/Header";
-import Footer from "../shared/components/layout/Footer";
-import Content from "../shared/components/layout/Content";
 import Popup from 'react-popup';
+import Header from '../shared/components/layout/Header';
+import Footer from '../shared/components/layout/Footer';
+import Content from '../shared/components/layout/Content';
 
-// import Home from '../components/Home/Home';
-// import Todo from "./Todo/Todo";
-// import Timer from "./Pomodoro/Timer";
-// import Coins from "./Coins/Coins";
-// import Notes from "./Notes/Notes";
-// import Chart from "./Chart/Chart";
-// import Animation from "./Animation/Animation";
-// import Numbers from "./Numbers/Numbers";
-//import Xss from "./Xss/Xss";
-// import Calculator from "./Calculator/Calculator";
+import Home from './Home/Home';
+import Todo from './Todo/Todo';
+import Timer from './Pomodoro/Timer';
+import Coins from './Coins/Coins';
+import Notes from './Notes/Notes';
+import Chart from './Chart/Chart';
+import Animation from './Animation/Animation';
+import Numbers from './Numbers/Numbers';
+import Xss from './Xss/Xss';
+import Calculator from './Calculator/Calculator';
 
 
 // This is our fake data...
-import {notes1, notes2} from './Notes/data';
-import Person from "./Person/Person";
+import { notes1, notes2 } from './Notes/data';
+import Person from './Person/Person';
 
 class App extends Component {
-    constructor() {
-        super();
-        // The first time we load the notes1...
-        this.state = {
-            notes: notes1,
-            chartType: 'line'
-        };
+  constructor() {
+    super();
+    // The first time we load the notes1...
+    this.state = {
+      notes: notes1,
+      chartType: 'line'
+    };
 
-        this.columns = [
-            ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
-            ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
-            ['XRP', 100, 200, 300, 500, 400, 300],
-        ];
-    }
+    this.columns = [
+      ['BTC', 3000, 6000, 10000, 15000, 13000, 11000],
+      ['ETH', 2000, 3000, 5000, 4000, 3000, 940],
+      ['XRP', 100, 200, 300, 500, 400, 300],
+    ];
+  }
+
+  componentDidMount() {
+    const { notes } = this.state;
+    // After 10 seconds (10000 milliseconds) we concatenate our
+    // data with notes2...
+    setTimeout(() => {
+      this.setState({
+        notes: [...notes, ...notes2]
+      });
+    }, 10000);
+  }
 
     setBarChart = () => {
-        this.setState({
-            chartType: 'bar'
-        });
+      this.setState({
+        chartType: 'bar'
+      });
     }
 
     setLineChart = () => {
-        this.setState({
-            chartType: 'line'
-        });
-    }
-
-    componentDidMount() {
-        // After 10 seconds (10000 milliseconds) we concatenate our
-        // data with notes2...
-        setTimeout(() => {
-            this.setState({
-                notes: [...this.state.notes, ...notes2]
-            });
-        }, 10000);
+      this.setState({
+        chartType: 'line'
+      });
     }
 
     render() {
-        return (
-            <div className="App">
-                <Header title="The new header title"/>
-                <Content>
-                    <Person/>
-                    {/*<Calculator/>*/}
-                    {/*<Xss/>*/}
-                    {/*<Numbers/>*/}
-                    {/*<Animation/>*/}
-                    {/*<Chart*/}
-                    {/*columns={this.columns}*/}
-                    {/*chartType={this.state.chartType}*/}
-                    {/*/>*/}
+      const { chartType, notes } = this.state;
+      return (
+        <div className="App">
+          <Header title="The new header title" />
+          <Content>
+            <Person />
+            <Calculator />
+            <Xss />
+            <Numbers />
+            <Animation />
+            <Chart
+              columns={this.columns}
+              chartType={chartType}
+            />
 
-                    {/*<p>*/}
-                    {/*Chart Type*/}
-                    {/*<button onClick={this.setBarChart}>Bar</button>*/}
-                    {/*<button onClick={this.setLineChart}>Line</button>*/}
-                    {/*</p>*/}
-                    {/*<Notes notes={this.state.notes}/>*/}
-                    {/*<Coins/>*/}
-                    {/*<Timer/>*/}
-                    {/*<Todo/>*/}
-                    {/*<Home/>*/}
-                </Content>
-                <Footer/>
-                <Popup/>
+            <p>
+                  Chart Type
+              <button type="button" onClick={this.setBarChart}>Bar</button>
+              <button type="button" onClick={this.setLineChart}>Line</button>
+            </p>
+            <Notes notes={notes} />
+            <Coins />
+            <Timer />
+            <Todo />
+            <Home />
+          </Content>
+          <Footer />
+          <Popup />
 
-            </div>
-        );
+        </div>
+      );
     }
 }
 
