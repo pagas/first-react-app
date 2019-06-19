6f871cb44a0b2f50315c3163cf66353dd44fd97c
paulius <paulius@2simple.com>
2019-06-19 20:59:29 +0100

    fix lint extension

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* eslint react/jsx-filename-extension: "off" */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -10,22 +12,22 @@ const unmountButton = document.getElementById('unmount');
 // but this will be just to understand how
 // componentWillUnmount works.
 function unmount() {
-    ReactDOM.unmountComponentAtNode(
-        document.getElementById('root')
-    );
+  ReactDOM.unmountComponentAtNode(
+    document.getElementById('root')
+  );
 
-    document.getElementById('unmountMessage')
-        .style.display = 'block';
+  document.getElementById('unmountMessage')
+    .style.display = 'block';
 
-    unmountButton.remove();
+  unmountButton.remove();
 }
 
 unmountButton.addEventListener('click', unmount);
 
 document.getElementById('unmountMessage')
-    .style.display = 'none';
+  .style.display = 'none';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
